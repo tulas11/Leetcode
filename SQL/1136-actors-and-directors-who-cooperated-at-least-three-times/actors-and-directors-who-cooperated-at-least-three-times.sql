ab32aa41c58c25975be00e0d038d8fa0982b2352
Yash Tulaskar <78523648+tulas11@users.noreply.github.com>
2024-04-11 23:40:25 +0530

    Time: 674 ms (63.55%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ SQL/1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,5 +1,9 @@
 # Write your MySQL query statement below
-select actor_id, director_id
-from actordirector
-group by actor_id, director_id
-having count(timestamp)>=3
+select actor_id, director_id 
+from(
+select actor_id,director_id, 
+count(timestamp) as cooperated 
+from ActorDirector 
+group by actor_id,director_id) 
+table1
+where cooperated>=3;
