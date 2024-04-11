5e720cd3807dae2d1a7e599a736e560161b021c9
Yash Tulaskar <78523648+tulas11@users.noreply.github.com>
2024-04-11 23:47:02 +0530

    Update customer-placing-the-largest-number-of-orders.sql

--- SQL/586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ SQL/586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -4,3 +4,15 @@ from orders
 group by customer_number
 order by count(*) desc
 limit 1
+
+-- SELECT customer_number
+-- FROM orders
+-- GROUP BY customer_number
+-- HAVING COUNT(*) = (
+--     SELECT MAX(order_count)
+--     FROM (
+--         SELECT COUNT(*) AS order_count
+--         FROM orders
+--         GROUP BY customer_number
+--     ) AS max_counts
+-- );
