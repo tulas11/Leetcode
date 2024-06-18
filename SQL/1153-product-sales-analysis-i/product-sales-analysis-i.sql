b349fa4c9328dce1a0cde0273d137849474581c5
Yash Tulaskar <78523648+tulas11@users.noreply.github.com>
2024-06-18 22:38:53 +0530

    Time: 788 ms (92.46%) | Memory: 0B (100.00%) - LeetSync

--- SQL/1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ SQL/1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,8 @@
 /* Write your PL/SQL query statement below */
-select p.product_name, s.year, s.price
-from product p inner join sales s on p.product_id=s.product_id
+SELECT 
+    (SELECT product_name FROM Product WHERE product_id = s.product_id) AS product_name,
+    s.year,
+    s.price
+FROM 
+    Sales s;
+	
